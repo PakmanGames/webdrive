5c06f0ff1ac21337f63c694844d71551ccf3fd0e
Andy Pak <im.andy.pak@gmail.com>
2025-06-08 21:13:13 -0400

    Create logic for client session with appwrite

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,28 @@
+import { Account, Client, Databases } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { cookies } from "next/headers";
+
+// Create client session instance
+export const createSessionClient = async () => {
+    const client = new Client().setEndpoint(appwriteConfig.endpointUrl).setProject(appwriteConfig.projectId);
+
+    const session = (await cookies()).get("appwrite-session");
+    if (!session || !session.value) {
+        throw new Error("No session found");
+    }
+
+    client.setSession(session.value);
+
+    return {
+        get account() {
+            return new Account(client);
+        },
+        get databases() {
+            return new Databases(client);
+        },
+    };
+};
+
+export const createAdminClient = async () => {
+    // const client = new Client();
+};
