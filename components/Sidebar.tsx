f3b10b86ebc7e5dc80b6467a6a58a6b6bc7a4a33
PakmanGames <im.andy.pak@gmail.com>
2025-08-02 22:20:44 -0400

    Create components for sidebar and a constants file

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Sidebar = () => {
-    return <div>Sidebar</div>;
+    return (
+        <aside className="sidebar">
+            <Link href="/">
+                <Image src="/assets/icons/logo-full-brand.svg" alt="logo" width={160} height={50} className="hidden h-auto lg:block" />
+                <Image src="/assets/icons/logo-brand.svg" alt="logo" width={52} height={52} className="lg:hidden" />
+            </Link>
+
+            <nav className="sidebar-nav">
+                <ul className="flex flex-1 flex-col gap-6"></ul>
+            </nav>
+        </aside>
+    );
 };
 
 export default Sidebar;
