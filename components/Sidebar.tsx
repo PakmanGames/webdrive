2504042abc67e243b1824a996261cf8cdcc39516
PakmanGames <im.andy.pak@gmail.com>
2025-08-04 15:15:49 -0400

    Add nav links to the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,14 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { navItems } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const Sidebar = () => {
+    const pathname = usePathname();
+
     return (
         <aside className="sidebar">
             <Link href="/">
@@ -11,8 +17,18 @@ const Sidebar = () => {
             </Link>
 
             <nav className="sidebar-nav">
-                <ul className="flex flex-1 flex-col gap-6"></ul>
+                <ul className="flex flex-1 flex-col gap-6">
+                    {navItems.map(({ url, name, icon }) => (
+                        <Link key={name} href={url} className="lg:w-full">
+                            <li className={cn("sidebar-nav-item", pathname === url && "shad-active")}>
+                                <Image src={icon} alt={name} width={24} height={24} className={cn("nav-icon", pathname === url && "nav-icon-active")} />
+                                <p className="hidden lg:block">{name}</p>
+                            </li>
+                        </Link>
+                    ))}
+                </ul>
             </nav>
+            <Image src="/assets/images/files-2.png" alt="logo" width={500} height={418} className="w-full" />
         </aside>
     );
 };
