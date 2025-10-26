bc4f5d63d87b2b59836f098b69e9309657de0bfc
PakmanGames <im.andy.pak@gmail.com>
2025-10-26 16:40:40 -0400

    Implement rename file functionality

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -16,6 +16,8 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { renameFile } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
 
 const ActionsDropdown = ({ file }: { file: Models.Document }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -23,6 +25,7 @@ const ActionsDropdown = ({ file }: { file: Models.Document }) => {
   const [action, setAction] = useState<ActionType | null>(null);
   const [name, setName] = useState(file.name);
   const [isLoading, setIsLoading] = useState(false);
+  const path = usePathname();
 
   const closeAllModals = () => {
     setIsModalOpen(false);
@@ -33,7 +36,27 @@ const ActionsDropdown = ({ file }: { file: Models.Document }) => {
     // TODO: reset emails for share
   };
 
-  const handleAction = async () => {};
+  const handleAction = async () => {
+    if (!action) {
+      return;
+    }
+    setIsLoading(true);
+    let success = false;
+
+    const actions = {
+      rename: () => renameFile({ fileId: file.$id, name, extension: file.extension, path }),
+      share: () => console.log("share"),
+      delete: () => console.log("delete"),
+    };
+
+    success = await actions[action.value as keyof typeof actions]();
+
+    if (success) {
+      closeAllModals();
+    }
+
+    setIsLoading(false);
+  };
 
   const renderDialogContent = () => {
     if (!action) {
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -7,7 +7,6 @@ import { ID, Models, Query } from "node-appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "@/lib/utils";
 import { revalidatePath } from "next/cache";
 import { getCurrentUser } from "@/lib/actions/user.actions";
-import { parse } from "path";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -76,5 +75,23 @@ export const getFiles = async () => {
     return parseStringify(files);
   } catch (error) {
     handleError(error, "Failed to get files");
+  }
+};
+// 9171;
+
+export const renameFile = async ({ fileId, name, extension, path }: RenameFileProps) => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const newName = `${name}.${extension}`;
+    const updatedFile = await databases.updateDocument(appwriteConfig.databaseId, appwriteConfig.filesCollectionId, fileId, {
+      name: newName,
+    });
+
+    revalidatePath(path);
+
+    return parseStringify(updatedFile);
+  } catch (error) {
+    handleError(error, "Failed to rename file");
   }
 };
