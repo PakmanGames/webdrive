25920e03856474c5c3c5e8b32e551051dd16d030
PakmanGames <im.andy.pak@gmail.com>
2025-10-26 11:19:08 -0400

    Implement file uploading functionality

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -5,6 +5,10 @@ import { Button } from "@/components/ui/button";
 import Thumbnail from "@/components/Thumbnail";
 import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
 import Image from "next/image";
+import { MAX_FILE_SIZE } from "@/constants";
+import { useToast } from "@/hooks/use-toast";
+import { uploadFile } from "@/lib/actions/file.actions";
+import { usePathname } from "next/navigation";
 
 interface Props {
   ownerId: string;
@@ -13,12 +17,40 @@ interface Props {
 }
 
 const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const path = usePathname();
+  const { toast } = useToast();
   const [files, setFiles] = useState<File[]>([]);
-  const onDrop = useCallback(async (acceptedFiles: File[]) => {
-    setFiles(acceptedFiles);
-  }, []);
+  const onDrop = useCallback(
+    async (acceptedFiles: File[]) => {
+      setFiles(acceptedFiles);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+      const uploadPromises = acceptedFiles.map(async (file) => {
+        if (file.size > MAX_FILE_SIZE) {
+          setFiles((prevFiles) => prevFiles.filter((f) => f.name !== file.name));
+
+          return toast({
+            description: (
+              <p className="body-2 text-white">
+                <span className="font-semibold">{file.name}</span> is too large. Max file size is 50MB.
+              </p>
+            ),
+            className: "error-toast",
+          });
+        }
+
+        return uploadFile({ file, ownerId, accountId, path }).then((uploadedFile) => {
+          if (uploadedFile) {
+            setFiles((prevFiles) => prevFiles.filter((f) => f.name !== file.name));
+          }
+        });
+      });
+
+      await Promise.all(uploadPromises);
+    },
+    [ownerId, accountId, path]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   const handleRemoveFile = (e: React.MouseEvent<HTMLImageElement, MouseEvent>, fileName: string) => {
     e.stopPropagation();
@@ -54,7 +86,6 @@ const FileUploader = ({ ownerId, accountId, className }: Props) => {
           })}
         </ul>
       )}
-      {isDragActive ? <p>Drop the files here...</p> : <p>Drag &apos;n&apos; drop some files here, or click to select files</p>}
     </div>
   );
 };
